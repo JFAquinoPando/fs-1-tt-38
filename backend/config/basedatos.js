0f450bfa79aa9c588d2b4da4dd00cf9096bb84b2
JFAquinoPando <pandolcik@gmail.com>
2026-01-21 21:30:48 -0300

    feat: actualizar configuración de conexión a MongoDB y agregar archivo de configuración para Vercel

--- backend/config/basedatos.js
+++ backend/config/basedatos.js
@@ -5,7 +5,7 @@ const baseDatos = "idt";
 
 export const conectarDB = async () => {
     try {
-        await mongoose.connect(`${url}/${baseDatos}`);
+        await mongoose.connect(`mongodb+srv://Vercel-Admin-proyectofullstack:7gxrs8LCgYgqvVbV@proyectofullstack.f8atc4p.mongodb.net/?retryWrites=true&w=majority`);
         console.log("✓ Conectado a MongoDB");
     } catch (error) {
         console.error("✗ Error al conectar a MongoDB:", error.message);
