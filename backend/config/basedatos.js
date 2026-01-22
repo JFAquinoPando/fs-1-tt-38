2c7afbc51311a43c0e177ba9f27f53a1d2eef727
JFAquinoPando <pandolcik@gmail.com>
2026-01-21 23:53:08 -0300

    feat: actualizar la conexión a MongoDB utilizando la variable de entorno MONGODB_URI y mejorar la gestión de conexiones

--- backend/config/basedatos.js
+++ backend/config/basedatos.js
@@ -1,16 +1,41 @@
 import mongoose from "mongoose";
 
-const url = "mongodb://localhost:27017";
-const baseDatos = "idt";
+// const url = "mongodb://localhost:27017";
+// const baseDatos = "idt";
 
-export const conectarDB = async () => {
+/* export const conectarDB = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://Vercel-Admin-proyectofullstack:7gxrs8LCgYgqvVbV@proyectofullstack.f8atc4p.mongodb.net/?retryWrites=true&w=majority`);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("✓ Conectado a MongoDB");
     } catch (error) {
         console.error("✗ Error al conectar a MongoDB:", error.message);
         process.exit(1);
     }
+}; */
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("❌ Falta la variable MONGODB_URI");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export const conectarDB = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+      console.log("✓ Conectado a MongoDB");
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default mongoose;
